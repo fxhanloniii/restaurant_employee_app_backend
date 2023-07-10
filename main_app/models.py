582bc994df67e6e51cc6d96d1f9bb87c3f193da3
fxhanloniii <hanlonfrankiex@gmail.com>
2023-07-10 13:06:20 -0700

    setting up backend

---
+++ main_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+# class MenuItem(models.Model):
+#     name = models.CharField(max_length=100)
+#     ingredients = models.CharField(max_length=250)
+#     description = models.TextField()
+#     course = models.CharField(max_length=50)
