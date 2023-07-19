678883f5f007b2d73f8371097a774fca485be504
fxhanloniii <hanlonfrankiex@gmail.com>
2023-07-19 11:40:28 -0700

    deploying

--- build.sh
+++ build.sh
@@ -1,24 +1,10 @@
-# #Install dependencies
-# pip3 install -r deps.txt
+#!/bin/bash
 
-# python manage.py collectstatic --no-input
-# #Run migrations
-# python3 manage.py migrate
-
-# #!/usr/bin/env bash
-# set -o errexit
-# pip install -r requirements.txt
-# python manage.py collectstatic --no-input
-# python manage.py migrate
-
-#!/usr/bin/env bash
-# set -o errexit
-# pip install -r requirements.txt
-# gunicorn restaurant_employee_backend_project.wsgi:application --bind 0.0.0.0:$PORT --workers 4
-
-#Install dependencies
+# Install dependencies
 pip3 install -r deps.txt
 
-python3 manage.py collectstatic --no-input
-#Run migrations
-python3 manage.py migrate
+# Run collectstatic
+python manage.py collectstatic --no-input
+
+# Run migrations
+python manage.py migrate
