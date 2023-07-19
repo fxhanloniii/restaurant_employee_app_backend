aa824d1ffdf4a64048dbedb8b9469ebb2422b531
fxhanloniii <hanlonfrankiex@gmail.com>
2023-07-19 11:46:26 -0700

    deploying

--- restaurant_employee_backend_project/settings.py
+++ restaurant_employee_backend_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
 import dj_database_url
 import cloudinary
 import cloudinary.uploader
@@ -21,10 +21,11 @@ import cloudinary.api
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
+SECRET_KEY = os.getenv('SECRET_KEY')
+DATABASE_URL = os.getenv('DATABASE_URL')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
