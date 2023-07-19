e6d11c7a9ea6149e52759f532677a888e4584228
fxhanloniii <hanlonfrankiex@gmail.com>
2023-07-19 11:39:23 -0700

    deploying

--- build.sh
+++ build.sh
@@ -19,6 +19,6 @@
 #Install dependencies
 pip3 install -r deps.txt
 
-python manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input
 #Run migrations
 python3 manage.py migrate
